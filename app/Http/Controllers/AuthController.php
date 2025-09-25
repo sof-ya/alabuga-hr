7e96b4508e3cc5aa6f9eef2e4a9b064462bc0db1
sofja.kulickowa <sofja.kulickowa@yandex.ru>
2025-09-26 01:06:02 +0300

    chore: пакет для генерации документации Swagger
    - Установка пакета для Swagger;
    - Начало документации; добаление аннотаций в основной контроллер и в контроллер авторизации для метода login;
    - Генерация файла json.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,14 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
-use InvalidArgumentException;
 use Laravel\Sanctum\NewAccessToken;
+use OpenApi\Attributes as OAT;
 
 class AuthController extends Controller
 {
+    #[OAT\Post(
+        path: '/api/auth/login',
+        description: 'Get token.',
+        requestBody: new OAT\RequestBody(
+                required: true,
+                content: new OAT\JsonContent(required: ['email', 'password'], properties: [
+                    new OAT\Property(property: 'email', type: 'string', format: 'email', example: 'test@example.com'),
+                    new OAT\Property(property: 'password', type: 'string', format: 'password', example: 'password'),
+                        ]),
+        ),
+        tags: ['auth'],
+        responses: [
+            new OAT\Response('', JsonResponse::HTTP_OK),
+            new OAT\Response('', JsonResponse::HTTP_UNPROCESSABLE_ENTITY),
+        ],
+
+    )]
     public function login() : JsonResponse {
         $credentials = request(['email', 'password']);
 
