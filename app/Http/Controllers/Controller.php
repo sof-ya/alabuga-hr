85653d75aabe3704cf2095d33eade890327d7555
sofja.kulickowa <sofja.kulickowa@yandex.ru>
2025-09-27 15:14:48 +0300

    chore: авторизации через JWT
    - Установка пакета для авторизации JWT;
    - Основной контроллер наследует базовый;
    - Создание контроллера автооризации JWT. Добавление методов основных методов;
    - Добавление методов JWT в модель пользователя;
    - Изменение конфигов авторизации и добавление конфига jwt;
    - Добавлены основные роуты в список роутов api.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use Illuminate\Routing\Controller as BaseController;
 use OpenApi\Attributes as OAT;
 
 #[OAT\Info(title: 'API', version: '1.0.0')]
-abstract class Controller
+abstract class Controller extends BaseController
 {
     //
 }
