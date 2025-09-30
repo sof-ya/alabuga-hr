4534a5442ea82f13df0bfe8dc8057c7bb3bf393e
Vitalij <krinickij.v.a@gmail.com>
2025-09-30 23:14:11 +0300

    FEAT: aded base category component for missions
    Добавлен базовый компонент для категории миссий
    Добавлен а иконка в спрайт

--- resources/js/Pages/MissionsPage.vue
+++ resources/js/Pages/MissionsPage.vue
@@ -1,12 +1,56 @@
 <template>
     <MainLayout>
-        страница MissionsPage.vue
+        <div class="pt-5 px-4 flex flex-col gap-2.5">
+            <div class="flex flex-row gap-2">
+                    <ButtonWithTwoStates
+                    text="Все"
+                    :state="activeTab === 'all'"
+                    class="!w-fit"
+                    @click="()=>toggleTabs('all')"
+                    />
+                    <ButtonWithTwoStates
+                    text="Активные"
+                    :state="activeTab === 'active'"
+                    class="!w-fit"
+                    @click="()=>toggleTabs('active')"
+                    />
+                    <ButtonWithTwoStates
+                    text="Завершённые"
+                    class="!w-fit"
+                    :state="activeTab === 'completed'"
+                    @click="()=>toggleTabs('completed')"/>
+            </div>
+            <div v-if="activeTab === 'all'" 
+            class="flex flex-col gap-2.5">
+                <MissionCategoryComponent
+                title="Название категории"
+                desc="Описание ветки спокойно адаптируется по высоте в зависимости от количества текста"
+                :progress="20"/>
+            </div>
+            <div 
+            v-if="activeTab === 'active'"
+            class="">
+                active
+            </div>
+            <div 
+            v-if="activeTab === 'completed'"
+            class="">
+                completed
+            </div>
+
+        </div>
     </MainLayout>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import MainLayout from '../components/Layout/MainLayout.vue';
-
+import ButtonWithTwoStates from '../components/ui/ButtonWithTwoStates.vue'
+import MissionCategoryComponent from '../components/pages/missionsPage/MissionCategoryComponent.vue';
+const activeTab=ref('all')
+const toggleTabs = (type)=>{
+    activeTab.value = type
+}
 </script>
 
 <style lang="scss" scoped>
---
+++ resources/js/components/pages/missionsPage/MissionCategoryComponent.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="wrapper">
+        <div class="header">
+            <div class="flex items-center gap-2">
+                <div class="h-7 w-7 rounded overflow-hidden bg-gray-300">
+
+                </div>
+                <h4>{{ props.title }}</h4>
+            </div>
+            <button @click="toggleActivity" class="h-6 w-6 ">
+                <IconComponent
+                name="check"
+                :class="{'rotate-180' : activity}"/>
+            </button>
+        </div>
+        <p class="description">{{ props.desc }}</p>
+        <div class="progress">
+            <div class="progress_persent" :style="`width: ${props.progress}%;`">{{ props.progress }}%</div>
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import IconComponent from '../../ui/IconComponent.vue';
+
+const activity = ref(false)
+const props = defineProps({
+    title:{
+        type:String,
+        required:true
+    },
+    desc:{
+        type:String,
+    },
+    progress:{
+        type:Number,
+        default: 0
+    }
+})
+const toggleActivity =()=>{
+    activity.value=!activity.value
+}
+</script>
+
+<style scoped>
+    .wrapper{
+        border-radius: 12px;
+        background: var(--white-500);
+        padding: 12px;
+        display: flex;
+        flex-direction: column;
+        gap: 4px;
+    }
+    .header{
+        display: grid;
+        grid-template-columns: 1fr 24px;
+        gap: 10px;
+        h4{
+            font-family: var(--font-family);
+            font-weight: 700;
+            font-size: 16px;
+            color: var(--white-950);
+        }
+    }
+    .description{
+        font-family: var(--font-family);
+        font-weight: 400;
+        font-size: 13px;
+        color: var(--white-950);
+    }
+    .progress{
+        background: var(--white-50);
+        width: 100%;
+        height: 11px;
+        border-radius: 80px;
+    }
+    .progress_persent{
+        font-family: var(--font-family);
+        font-weight: 400;
+        font-size: 8px;
+        color: var(--blue-700);
+        background: var(--blue-400);
+        border-radius: 40px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+</style>
