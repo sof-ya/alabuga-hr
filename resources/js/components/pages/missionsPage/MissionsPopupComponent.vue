512099caaff516045f38beaf1d8ba6fdd4f27842
Vitalij <krinickij.v.a@gmail.com>
2025-10-01 23:52:31 +0300

    FEAT: edited outpua actual data to MissionsPopupComponent

--- resources/js/components/pages/missionsPage/MissionsPopupComponent.vue
+++ resources/js/components/pages/missionsPage/MissionsPopupComponent.vue
@@ -8,24 +8,25 @@
             </div>
 <div v-else-if="missionsState.branchProgress" class="flex flex-col gap-4">
     <div class="flex flex-col gap-1">
-        <h2 class="title">Название категории</h2>
-        <p class="desc">Описание категории если надо</p>
+        <h2 class="title">{{ missionsState.branchProgress.current_branch.name }}</h2>
+        <p class="desc">{{ missionsState.branchProgress.current_branch.description }}</p>
     </div>
     <div class="">
         <h2 class="title">Требования к описанию:</h2>
-    </div>
-    <div class=" grid grid-cols-4">
-        <div class="flex flex-col">
-            <h3 class="subtitle">Роль</h3>
-            <span class="requirements">Название</span>
+        <div class=" grid grid-cols-4">
+            <div class="flex flex-col">
+                <h3 class="subtitle">Роль</h3>
+                <span class="requirements">{{ missionsState.branchProgress.role.description || 'Без роли' }}</span>
+            </div>
+            <div class="flex flex-col">
+                <h3 class="subtitle">Опыт</h3>
+                <span class="requirements">{{ missionsState.branchProgress.experience || '0' }} HP</span>
+            </div>
         </div>
-        <div class="flex flex-col">
-            <h3 class="subtitle">Опыт</h3>
-            <span class="requirements">3000 HP</span>
-        </div>
     </div>
+    
     <div class="flex flex-wrap gap-8">
-        <div class="">
+        <div class="" v-if="false">
             <h3 class="subtitle">Компетенции</h3>
             <ul>
                 <li>Название компетенции:</li>
@@ -33,20 +34,29 @@
                 <li>Название компетенции:</li>
             </ul>
         </div>
-        <div class="">
+        <div class="" v-if="missionsState.branchProgress.branches.length">
             <h3 class="subtitle">Завершенные ветки</h3>
             <ul>
-                <li>Название компетенции:</li>
-                <li>Название компетенции:</li>
-                <li>Название компетенции:</li>
+                <li 
+                v-for="elem in missionsState.branchProgress.branches"
+                :class="{
+                    'text-green-600':elem.is_completed,
+                    'text-red-600':!elem.is_completed,
+                }"
+                >{{ elem.branch_name}}: {{ elem.status_display }}</li>
+
             </ul>
         </div>
-        <div class="">
+        <div class="" v-if="missionsState.branchProgress.missions.length">
             <h3 class="subtitle">Завершенные миссии</h3>
             <ul>
-                <li>Название компетенции:</li>
-                <li>Название компетенции:</li>
-                <li>Название компетенции:</li>
+                <li 
+                v-for="elem in missionsState.branchProgress.missions"
+                :class="{
+                    'text-green-600':elem.is_completed,
+                    'text-red-600':!elem.is_completed,
+                }"
+                >{{ elem.name }}: {{elem.is_completed ? 'пройдена' : 'не пройдена' }}</li>
             </ul>
         </div>
     </div>
@@ -90,17 +100,11 @@ ul{
     display: flex;
     flex-direction: column;
     gap: 16px;
+    margin-top: 6px;
     li{
         font-family: var(--font-family);
         font-weight: 400;
         font-size: 10px;
-
-    }
-    li:nth-child(odd){
-        color: var(--red-500);
-    }
-    li:nth-child(even){
-        color: var(--green-500);
     }
 }
 </style>
